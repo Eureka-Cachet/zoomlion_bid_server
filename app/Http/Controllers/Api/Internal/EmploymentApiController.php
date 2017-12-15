ea4f403f3feb16f1f22c861db7a0b120ebc63aee
Thomas Paulson <thomasdolar@gmail.com>
2017-12-15 07:32:48 +0000

    single thread generation of form for testing purpose

--- app/Http/Controllers/Api/Internal/EmploymentApiController.php
+++ app/Http/Controllers/Api/Internal/EmploymentApiController.php
@@ -13,6 +13,9 @@ use clocking\Department;
 use clocking\Http\Controllers\Controller;
 use clocking\Jobs\BuildApplicantForm;
 use clocking\Module;
+use Eureka\Helpers\CodeGenerator;
+use Eureka\Helpers\Config;
+use Eureka\Repositories\BeneficiaryRepository;
 use Eureka\Repositories\FormRepository;
 use Illuminate\Http\Request;
 use Illuminate\Support\Collection;
@@ -39,11 +42,57 @@ class EmploymentApiController extends Controller
     public function generate_form(Request $request)
     {
         try{
-            $this->dispatch(new BuildApplicantForm($request->all(), auth()->user()));
-            return $this->onSuccessRequest();
+//            $this->dispatch(new BuildApplicantForm($request->all(), auth()->user()));
+
+            $number_to_generate = $request->get('number_of_form');
+            $forms = $this->generate_them($number_to_generate, auth()->user(), $request);
+            return $forms;
+
+//            return $this->onSuccessRequest();
         }catch (\Exception $e){
             return $this->onFailedRequest($e->getMessage());
         }
+    }
+
+    private function generate_them($number_to_generate, $generator, $request)
+    {
+        $forms = [];
+        for($increment = 0; $increment < $number_to_generate; $increment++){
+            $single_form_data = $this->build_form_data($generator->id, $increment + 1, $request);
+            $saved_form = Form::create($single_form_data);
+            array_push($forms, $saved_form);
+        }
+        return $forms;
+    }
+
+    private function build_form_data($generator_id, $increment, $request)
+    {
+        return [
+            'code' => $this->generate_form_code($increment),
+            'uuid' => CodeGenerator::uuid(),
+            'module_id' => $request->get('module_id'),
+            'rank_id' => $request->get('rank_id'),
+            'user_id' => $generator_id
+        ];
+    }
+
+    private function generate_form_code($increment)
+    {
+        // ZGH0123456
+        $initials = Config::getInitials();
+        $suffix = $this->make_suffix($increment);
+        return "{$initials}{$suffix}";
+    }
+
+    /**
+     * @param $increment
+     * @return string
+     */
+    private function make_suffix($increment)
+    {
+        $bid = BeneficiaryRepository::get_new_bid($increment);
+        dump($bid);
+        return $bid;
     }
 
 }
