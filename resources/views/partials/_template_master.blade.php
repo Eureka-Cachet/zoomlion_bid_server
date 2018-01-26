fb199a1c14573e9d74567a8c1b1f862f738f87c2
Thomas Paulson <thomasdolar@gmail.com>
2018-01-26 11:22:24 +0000

    fixed pdf report table overlap

--- resources/views/partials/_template_master.blade.php
+++ resources/views/partials/_template_master.blade.php
@@ -4,9 +4,18 @@
     <head>
         @yield('style-content')
         <style>
-         .page-break {
-                page-break-after: always;
-            }
+             .page-break {
+                    page-break-after: always;
+                }
+             thead {
+                 display: table-header-group;
+             }
+             tfoot {
+                 display: table-row-group;
+             }
+             tr {
+                 page-break-inside: avoid;
+             }
         </style>
     </head>
     @yield('body-content')
